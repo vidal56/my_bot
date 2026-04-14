63a65c76b6a7b8b037718878714731c8ac035b10
vidal56 <gabrielnext287@gmail.com>
2026-04-14 08:55:56 -0300

    adicionando odom e suas referencias

--- src/my_bot/launch/launch_sim.launch.py
+++ src/my_bot/launch/launch_sim.launch.py
@@ -1,39 +1,51 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 def generate_launch_description():
-
-    # 1. NOME DO SEU PACOTE (Ajustado para o que você está usando)
     package_name='my_bot' 
 
-    # 2. Lança o Robot State Publisher (RSP)
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # 3. Lança o Gazebo Harmonic (Substituído gazebo_ros por ros_gz_sim)
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'
                 )]), launch_arguments={'gz_args': '-r empty.sdf'}.items()
     )
 
-    # 4. Nó de Spawn (Substituído spawn_entity.py pelo comando 'create' do Jazzy)
     spawn_entity = Node(package='ros_gz_sim', executable='create',
                         arguments=['-topic', 'robot_description',
                                    '-name', 'my_bot'],
                         output='screen')
 
+    # BRIDGE CONFIGURADO PARA JAZZY + HARMONIC
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
+            '/odom@nav_msgs/msg/Odometry[gz.msgs.Odometry',
+            '/tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V',
+            '/tf_static@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V',
+            '/joint_states@sensor_msgs/msg/JointState[gz.msgs.Model',
+            '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'
+        ],
+        parameters=[{
+            'qos_overrides./tf_static.publisher.durability': 'transient_local',
+        }],
+        output='screen'
+    )
+    
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        bridge,
     ])
